3a192895398ab8a2a122a0e4fd2aa4eae0076657
Bálint Szeleczky <szybalint@gmail.com>
2021-02-23 14:11:36 +0100

    Started complete rework

--- jumpcutter.py
+++ jumpcutter.py
@@ -62,7 +62,7 @@ def createPath(s):
     try:
         os.mkdir(s)
     except OSError:
-        assert False, "Creation of the directory %s failed. (The TEMP folder may already exist. Delete or rename it, and try again.)"
+        assert False, "Creation of the directory " + s + " failed. (The TEMP folder may already exist. Delete or rename it, and try again.)"
 
 
 def deletePath(s):  # Dangerous! Watch out!
